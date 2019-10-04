84417868ccbdf7d753fc512c7881ca04c617e9fe
Shubham Kumar <hello@shubhkumar.in>
2019-10-04 17:02:32 +0530

    Added caller index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const helper = require("./helper.js")
+
+helper.getLinks("https://gen.medium.com/getting-lost-in-the-subtlety-of-the-landscape-fd482179acde")
+
+// helper.getLinks('https://shubhkumar.in')
